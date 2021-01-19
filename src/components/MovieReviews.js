82724a66fe86bf37bf3597488d32ab9c4811e454
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-19 14:18:15 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,10 +1,31 @@
 // Code MovieReviews Here
+// Code MovieReviews Here
 import React from 'react';
 
-const MovieReviews = () => {
-  return(
-    <ul>
-    </ul>
-  )
-}
-export default MovieReviews
+const MovieReviews = ({ reviews }) => {
+return(
+<div className="list">{reviews.map(review => {
+    return (
+        <div>
+            <header>
+        <a
+          href={review.link.url}
+        >
+          {review.headline}
+        </a>
+        <br/>
+        <span>Title: {review.display_title}</span>
+        <br/>
+        <span >Author: {review.byline}</span>
+      </header>
+      <p>Summary: {review.summary_short}</p>
+        </div>
+    )
+})}</div>
+)};
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
