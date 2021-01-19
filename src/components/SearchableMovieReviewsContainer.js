6628e0eda47f17e26838dd58807141acbfdfd0d4
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-19 12:48:18 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,10 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends React.Components{
+  
+  render(){
+    return(null)
+  }
+}
+export default SearchableMovieReviewsContainer
